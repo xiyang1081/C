a0ce8f58fc6beee2e632e0f9c0791fb7de1023cf
夕阳 <linandli@yeah.net>
2015-02-10 10:42:45 +0800

    2
    2

---
+++ 2.c
@@ -0,0 +1,24 @@
+//main.c
+
+#include "sayhello.h"
+
+int main()
+{
+	SayHello();
+	printf("ssss\n");
+	return 0;
+}
+
+//sayhello.c
+#include "sayhello.h"
+
+void SayHello()
+{
+	printf("Hello,world!\n");
+}
+
+//sayhello.h
+
+#include<stdio.h>
+
+void SayHello();
